332e78f86b20931459c05c7304d5d059f8c3b498
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-04-21 19:25:22 +0300

    hw_18_done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Contacts from './contents/Contacts';
+
+function App() {
+  return (
+    <div className="App">
+      <Contacts/>
+      
+    </div>
+  );
+}
+
+export default App;
